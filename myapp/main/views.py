a87ee4ec3f9392616e9844e69f0f328c4c789ac5
John <lolznha72@gmail.com>
2025-12-02 09:15:27 +0700

    python and tailwind

---
+++ myapp/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.utils import timezone
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html', {
+        'current_time': timezone.now()
+    })
